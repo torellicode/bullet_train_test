75f2cee84b252f400a326a344a28a445f36d3922
torellicode <torellicode@gmail.com>
2025-03-13 02:39:58 -0400

    Changed environments/development to allow host connections from preview.torelli.dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # allow dev server connection
+  config.hosts << "preview.torelli.dev"
+
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
